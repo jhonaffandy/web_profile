d7ac526eb0b9eb5062f22a07cbf47c8c50a6358f
Johan <joe.affandy@gmail.com>
2023-04-30 17:19:37 +0700

    update comment 1

--- index.php
+++ index.php
@@ -9,7 +9,10 @@ echo $name == 'echo' ? 'ok' : "no";
 // git clone (git@github.com:jhonaffandy/web_profile.git)
 // git fetch origin (branch) 
 // git pull origin (branch)
+
 // git checkout (branch)
 // git merge 
+
 // git add (. || filename)
 // git commit -m '(comment)'
+// git push origin (branch)
